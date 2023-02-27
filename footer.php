0cf330f7b3a38b7f7b9da38c3e55e91c9799fb70
Saradwata-Bandyopadhyay <43028785+Saradwata-Bandyopadhyay@users.noreply.github.com>
2023-02-27 11:46:00 +0530

    Update footer.php
    The year of copyright get updated automatically.

--- footer.php
+++ footer.php
@@ -1,6 +1,10 @@
 <footer class="page-footer font-small bg-warning">
     <div class="footer-copyright text-center py-3">
-        © 2022 Copyright : VHDL Codes Forum
+    <?php 
+    $year = date("Y");
+    echo "©  $year";
+    ?> 
+    Copyright : VHDL Codes Forum
     </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  

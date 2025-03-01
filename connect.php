b9f013e7e26f2e0c1eafe67df74c3e244c3f934b
Saradwata-Bandyopadhyay <saradwatabandyopadhyay@gmail.com>
2025-03-01 22:25:23 +0530

    db connection updated

--- connect.php
+++ connect.php
@@ -1,6 +1,11 @@
 <?php
-$conn = mysqli_connect('localhost', 'root','1234', "vhdl");
-	if(!$conn){
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+$dbname = "vhdl";
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+	if(!$conn)
+	{
 		echo 'Connection error: '. mysqli_connect_error();
 	}
 ?>
